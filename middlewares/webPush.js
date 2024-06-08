06a21ab6ebc648b67ba1e75b94123897affdd63c
anhld2998 <anh.leduc2512@gmail.com>
2024-06-08 12:02:30 +0700

    fixx

--- middlewares/webPush.js
+++ middlewares/webPush.js
@@ -1,24 +1,19 @@
 const webPush = require('web-push');
-
 const vapidKeys = {
   publicKey: 'BA5xnicrkH0hO5H0Y3cK5AAik0G_j62c8mukA0eYjhQ9ShDxDvh9gksXEL-VRMoZaeT2bDh2y_1Wi2C3ro9d_9E',
   privateKey: 'rDJFQnxX2BgHuC_H_zlFTk_QpDQyOzP1eON4xu7ucP4'
 };
-
 webPush.setVapidDetails(
   'mailto:anh.leduc2512@gmail.com',
   vapidKeys.publicKey,
   vapidKeys.privateKey
 );
-
 const subscriptions = [];
-
 const saveSubscription = (req, res) => {
   const subscription = req.body;
   subscriptions.push(subscription);
   res.status(201).json({});
 };
-
 const sendNotification = (req, res) => {
   const { title, message } = req.body;
 
