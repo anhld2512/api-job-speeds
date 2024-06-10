6f7a3b7ffba4a6bf2054db76322d8d45ce35f7e0
AnhLD <anh.leduc2512@gmail.com>
2024-06-10 19:32:16 +0700

    fixx toute

--- routes/fileRoutes.js
+++ routes/fileRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const fileController = require('./controllers/fileController');
+const fileController = require('../controllers/fileController');
 
 router.post('/upload', fileController.uploadFile, fileController.handleFileUpload);
 router.get('/files/:filename', fileController.getFile);
