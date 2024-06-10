9337186705e186cdb992ce2df507eb5de24fcc65
AnhLD <anh.leduc2512@gmail.com>
2024-06-10 18:33:37 +0700

    update

--- server.js
+++ server.js
@@ -42,6 +42,9 @@ const corsOptions = {
   credentials: true,
 };
 app.use(cors(corsOptions));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(bodyParser.json({ limit: '50mb' }));
 app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
 
